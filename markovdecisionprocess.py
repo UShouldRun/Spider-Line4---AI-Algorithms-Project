b428d86ace429b3756145d5ac5b8899ac70aa81c
Henrique dos Santos Teixeira <116520755+UShouldRun@users.noreply.github.com>
2024-04-11 11:21:25 +0100

    Update markovdecisionprocess.py

--- markovdecisionprocess.py
+++ markovdecisionprocess.py
@@ -1,12 +1,10 @@
 class MDP: # markov decision process
-    def __init__(self, get_actions, state_analysis, execute, qfunction, const):
+    def __init__(self, get_actions, state_analysis, execute, qfunction):
         self._get_actions = get_actions
         self.state_analysis = state_analysis
         self.execute = execute
         self.qfunction = qfunction
-        self.const = const
         self.action_type = None
 
     def get_actions(self, node): return self._get_actions(node, self.action_type)
-    def get_const(self): return self.const
     def non_terminal(self, node): return not self.state_analysis(node)
