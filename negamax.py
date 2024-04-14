e589129af81995d5d00efce3701144243afe940a
Henrique dos Santos Teixeira <116520755+UShouldRun@users.noreply.github.com>
2024-04-14 02:57:09 +0100

    Update negamax.py

--- negamax.py
+++ negamax.py
@@ -60,4 +60,3 @@ class Negamax:
     def watch_stats(self, root) -> None:
         print(f"Total explored nodes: {self.nodes_depth}")
         print(f"Total created nodes: {Node.next_node_id - 1}")
-        print("-----------------//----------------")
